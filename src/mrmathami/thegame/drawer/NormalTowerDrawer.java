4b3746adc12fefb391938c2e45f819cdee99fa67
eruchii <nct1120@gmail.com>
2019-11-06 11:16:38 +0700

    Revert "revert 1"
    This reverts commit 5906bfa90c34ccdadca154b9981f9a317a259c99.

--- src/mrmathami/thegame/drawer/NormalTowerDrawer.java
+++ src/mrmathami/thegame/drawer/NormalTowerDrawer.java
@@ -12,7 +12,7 @@ import javax.annotation.Nonnull;
 public final class NormalTowerDrawer implements EntityDrawer {
 	@Override
 	public void draw(long tickCount, @Nonnull GraphicsContext graphicsContext, @Nonnull GameEntity entity, double screenPosX, double screenPosY, double screenWidth, double screenHeight, double zoom) {
-		graphicsContext.setFill(Color.RED);
+		graphicsContext.setFill(Color.WHITE);
 		graphicsContext.fillOval(screenPosX, screenPosY, screenWidth, screenHeight);
 	}
 }
