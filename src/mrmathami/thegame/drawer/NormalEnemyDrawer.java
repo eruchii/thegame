946bd5294008cdfa9fc5cea63b485599418ce14f
eruchii <nct1120@gmail.com>
2019-11-06 11:19:22 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/eruchii/thegame""
    This reverts commit c6aca720c02d3bc21c15feb6246e5361ffbb3be9.

--- src/mrmathami/thegame/drawer/NormalEnemyDrawer.java
+++ src/mrmathami/thegame/drawer/NormalEnemyDrawer.java
@@ -1,18 +1,35 @@
 package mrmathami.thegame.drawer;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.RadialGradient;
 import javafx.scene.paint.Stop;
+import mrmathami.thegame.Config;
 import mrmathami.thegame.entity.GameEntity;
+import mrmathami.thegame.entity.enemy.AbstractEnemy;
 
 import javax.annotation.Nonnull;
 
 public final class NormalEnemyDrawer implements EntityDrawer {
 	@Override
 	public void draw(long tickCount, @Nonnull GraphicsContext graphicsContext, @Nonnull GameEntity entity, double screenPosX, double screenPosY, double screenWidth, double screenHeight, double zoom) {
+		double percent = 1.0 * ((AbstractEnemy) entity).getHealth() / ((AbstractEnemy) entity).getMaxHealth();
+		graphicsContext.setFill(Color.RED);
+		graphicsContext.fillRect(screenPosX - 5, screenPosY - 8, (screenWidth + 10)*percent, 5);
+		graphicsContext.setStroke(Color.BLACK);
+		graphicsContext.setLineWidth(1);
+		graphicsContext.strokeRect(screenPosX - 5, screenPosY - 8, screenWidth + 10, 5);
+
 		graphicsContext.setFill(Color.DARKMAGENTA);
 		graphicsContext.fillRoundRect(screenPosX, screenPosY, screenWidth, screenHeight, 4, 4);
+
+//		String path = String.valueOf(this.getClass().getResource("/PNG/towerDefense_tile245.png"));
+//		double scale = 1;
+//		double newW = Config.NORMAL_ENEMY_SIZE * Config.TILE_SIZE * scale;
+//		double newH = Config.NORMAL_ENEMY_SIZE * Config.TILE_SIZE * scale;
+//		Image content = new Image(path, newW, newH, false, false);
+//		graphicsContext.drawImage(content, screenPosX, screenPosY);
 	}
 }
--- src/mrmathami/thegame/entity/bullet/AbstractBullet.java
+++ src/mrmathami/thegame/entity/bullet/AbstractBullet.java
@@ -1,5 +1,6 @@
 package mrmathami.thegame.entity.bullet;
 
+import mrmathami.thegame.Config;
 import mrmathami.thegame.GameField;
 import mrmathami.thegame.entity.*;
 import mrmathami.thegame.entity.enemy.AbstractEnemy;
@@ -39,8 +40,9 @@ public abstract class AbstractBullet extends AbstractEntity implements Updatable
 
 	protected void calcNewDelta(){
 		if(this.enemy != null) {
-			double diffX = enemy.getPosX() + enemy.getWidth() / 2.0 - this.getPosX() - this.getWidth()/2;
-			double diffY = enemy.getPosY() + enemy.getHeight() / 2.0 - this.getPosY() - this.getHeight()/2;
+			double scale = 0.8;
+			double diffX = enemy.getPosX() + enemy.getWidth()*scale  - this.getPosX();
+			double diffY = enemy.getPosY() + enemy.getHeight()*scale - this.getPosY();
 			double s = Math.sqrt(diffX * diffX + diffY * diffY);
 			double normalize = this.speed / s;
 			this.deltaX = diffX * normalize;
