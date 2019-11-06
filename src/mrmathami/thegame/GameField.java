8ddc53cad05840c60cfdf89194006e745b33cc23
eruchii <nct1120@gmail.com>
2019-11-06 11:16:14 +0700

    Revert "revert 2"
    This reverts commit f2eabee99634c3d49ef7a4526ec1821cb025e150.

--- src/mrmathami/thegame/GameField.java
+++ src/mrmathami/thegame/GameField.java
@@ -141,7 +141,6 @@ public final class GameField {
 				}
 			}
 		}
-
 		// 1.3. Update DestroyableEntity
 		final List<GameEntity> destroyedEntities = new ArrayList<>(Config._TILE_MAP_COUNT);
 		for (final GameEntity entity : entities) {
--- src/mrmathami/thegame/drawer/SmallerEnemyDrawer.java
+++ src/mrmathami/thegame/drawer/SmallerEnemyDrawer.java
@@ -1,6 +1,7 @@
 package mrmathami.thegame.drawer;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import mrmathami.thegame.entity.GameEntity;
 import mrmathami.thegame.entity.LivingEntity;
@@ -8,22 +9,27 @@ import mrmathami.thegame.entity.enemy.AbstractEnemy;
 import mrmathami.thegame.entity.enemy.SmallerEnemy;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 
 public final class SmallerEnemyDrawer implements EntityDrawer {
 	@Override
-	public void draw(long tickCount, @Nonnull GraphicsContext graphicsContext, @Nonnull GameEntity entity, double screenPosX, double screenPosY, double screenWidth, double screenHeight, double zoom) {
+	public void draw(long tickCount,
+					 @Nonnull GraphicsContext graphicsContext,
+					 @Nonnull GameEntity entity,
+					 double screenPosX,
+					 double screenPosY,
+					 double screenWidth,
+					 double screenHeight,
+					 double zoom) {
 		double percent = 1.0 * ((AbstractEnemy) entity).getHealth() / ((AbstractEnemy) entity).getMaxHealth();
+
 
 		graphicsContext.setFill(Color.RED);
 		graphicsContext.fillRect(screenPosX - 5, screenPosY - 8, (screenWidth + 10)*percent, 5);
 		graphicsContext.setStroke(Color.BLACK);
 		graphicsContext.setLineWidth(1);
-<<<<<<< HEAD
 		graphicsContext.strokeRect(screenPosX - 5, screenPosY - 8, screenWidth + 10, 5);
 
-=======
-		graphicsContext.strokeRect(screenPosX, screenPosY - 8, screenWidth, 5);
->>>>>>> parent of ea8740b... Testing
 		graphicsContext.setFill(Color.MAGENTA);
 		graphicsContext.fillRoundRect(screenPosX, screenPosY, screenWidth, screenHeight, 4, 4);
 	}
